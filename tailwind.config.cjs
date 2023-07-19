faea50c6cb718165fa8e7fe7700b6566781a3961
luco17 <lucodonoghue@gmail.com>
2023-07-19 16:21:25 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      title: ["Ultra"],
+      hand: ["Permanent Marker"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
