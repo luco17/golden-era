9115afa1d74665ad5d76ca8252a15d1116878cee
luco17 <lucodonoghue@gmail.com>
2023-07-20 13:16:22 +0100

    added alpine + dynamic workout changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()]
 });
