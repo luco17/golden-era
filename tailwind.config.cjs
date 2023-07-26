3d6160b275c3a87acf8e81c523d4bc7e4efbb527
luco17 <lucodonoghue@gmail.com>
2023-07-26 14:55:31 +0100

    added animations to reduce flashes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,33 @@ module.exports = {
       hand: ["Permanent Marker"],
       sans: ["Helvetica Neue"],
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeUp: {
+          "0%": { transform: "translateY(20px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fadeInLeft: {
+          "0%": { transform: "translateX(-300px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fadeInRight: {
+          "0%": { transform: "translateX(300px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fadeOpacity: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeOpacity 0.4s ease forwards 0.5s",
+        fadeFromLeft:
+          "fadeInLeft 0.8s ease forwards 0.5s, fadeOpacity 0.4s ease forwards 0.5s",
+        fadeFromRight:
+          "fadeInRight 0.8s ease forwards 0.5s, fadeOpacity 0.4s ease forwards 0.5s",
+      },
+    },
   },
   plugins: [],
 };
